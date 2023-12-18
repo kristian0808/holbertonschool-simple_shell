3757c4c644342bc5e05372cefc8ea51d53feb52e
a7il10 <atilio.sulovari199@gmail.com>
2023-12-18 01:49:12 -0800

    _printenv and _getenv functions

---
+++ env_utils.c
@@ -0,0 +1,50 @@
+#include "shell.h"
+
+/**
+ * _printenv - prints the environment
+ * @void: void
+ * Return: 0 on success, -1 on failure
+ */
+
+int _printenv(void)
+{
+	int i = 0;
+	char *str = environ[0];
+
+	if (str == NULL)
+	{
+		fprintf(stderr, "Environment variable is null");
+		return (-1);
+	}
+	while (str != NULL)
+	{
+		printf("%s\n", str);
+		i++;
+		str = environ[i];
+	}
+	return (0);
+}
+
+/**
+ * _getenv - Gets the value of an environment variable
+ * @var: the environment variable to get
+ * Return: the value of the environment variable
+ */
+
+char *_getenv(char *var)
+{
+	int i, len_var;
+
+	if (environ == NULL)
+	{
+		fprintf(stderr, "Environment variable is null");
+		return (NULL);
+	}
+	len_var = strlen(var);
+	for (i = 0; environ[i] != NULL; i++)
+	{
+		if (strncmp(environ[i], var, len_var) == 0)
+			return (&environ[i][len_var]);
+	}
+	return (NULL);
+}

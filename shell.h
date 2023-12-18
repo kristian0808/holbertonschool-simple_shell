720694d70083049b99be9d769dab2bb245cf5a42
Remz97 <remzikeqi@icloud.com>
2023-12-18 02:10:42 -0800

    Functions added

---
+++ shell.h
@@ -0,0 +1,23 @@
+#ifndef SHELL_H
+#define SHELL_H
+
+#include <stdio.h>
+#include <string.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <sys/stat.h>
+
+extern char **environ;
+
+/* Environment utilities */
+int _printenv(void);
+char *_getenv(char *var);
+
+/* Command path */
+char *command_path(char *cmd);
+int command_read(char *s); 
+int execute(char *cmd_arr[]);
+
+#endif

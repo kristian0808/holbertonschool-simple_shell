37096d4feffdc48c9c56370ba7cc23511e8d05fd
kristian0808 <kristianlleshaj@hotmail.com>
2023-12-20 14:12:06 -0800

    Fixed coding style

--- command_execute.c
+++ command_execute.c
@@ -13,7 +13,8 @@ int execute(char *cmd_array[])
 	char *exe_path = NULL;
 	char *cmd = NULL;
 	pid_t pid;
-	int status;	
+	int status;
+
 	cmd = cmd_array[0];
 	exe_path = command_path(cmd);
 	if (exe_path == NULL)

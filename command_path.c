e9799f4014bbaab8e4b15ce084b4fd36147ff3ec
kristian0808 <kristianlleshaj@hotmail.com>
2023-12-20 13:50:44 -0800

    Fixed command path

--- command_path.c
+++ command_path.c
@@ -1,49 +1,51 @@
 #include "shell.h"
+
 /**
- * command_path - finds the path from environ
- * @cmd: shell command
- * Return: full path of the command
+ * command_path - Finds the full path of a command
+ * @cmd: The command to find
+ * Return: The full path of the command
  */
+
 char *command_path(char *cmd)
 {
-	char *path = strdup(_getenv("PATH"));
-	char *token = strtok(path, ":");
-	char *path_array[100], *new_path = NULL;
-	int i = 0;
+	char *path, *path_copy, *full_path, *token;
 	struct stat buf;
 
-	new_path = malloc(sizeof(char) * 100);
-	if (_getenv("PATH")[0] == ':')
-		if (stat(cmd, &buf) == 0)
-			return (strdup(cmd));
-
+	path = _getenv("PATH");
+	if (path == NULL)
+	{
+		fprintf(stderr, "Path variable not found");
+		return (NULL);
+	}
+	path_copy = strdup(path);
+	if (path_copy == NULL)
+	{
+		fprintf(stderr, "Error copying path");
+		return (NULL);
+	}
+	token = strtok(path_copy, ":");
 	while (token != NULL)
 	{
-		path_array[i] = token;
-		i++;
+		full_path = malloc(strlen(token) + strlen(cmd) + 2);
+		if (full_path == NULL)
+		{
+			fprintf(stderr, "Error allocating full path");
+			free (path_copy);
+			return (NULL);
+		}
+		strcpy(full_path, token);
+		strcat(full_path, "/");
+		strcat(full_path, cmd);
+		if (stat(full_path, &buf) == 0)
+		{
+			free (path_copy);
+			return (full_path);
+		}
+		free (full_path);
 		token = strtok(NULL, ":");
 	}
-	path_array[i] = NULL;
-
-	for (i = 0; path_array[i]; i++)
-	{
-		strcpy(new_path, path_array[i]);
-		strcat(new_path, "/");
-		strcat(new_path, cmd);
-		strcat(new_path, "\0");
-		if (stat(new_path, &buf) == 0)
-		{
-			free(path);
-			return (new_path);
-		}
-		else
-			new_path[0] = 0;
-	}
-
-	free(path);
-	free(new_path);
+	free (path_copy);
 	if (stat(cmd, &buf) == 0)
 		return (strdup(cmd));
-
 	return (NULL);
 }

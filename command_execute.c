788b1e64a450af0dd6af8a78cfb160cea3587149
kristian0808 <kristianlleshaj@hotmail.com>
2023-12-20 04:09:05 -0800

    Update code

--- command_execute.c
+++ command_execute.c
@@ -3,47 +3,40 @@
 #include<stdio.h>
 #include <sys/wait.h>
 #include<stdlib.h>
-
 /**
- * execute -substitutes child process with respective program
- * @cmd_arr[]: - points to the array of pointers that point to respective string of command
- * Return: (0)
+ * execute - Function that executes the shell
+ * @cmd_array: First operand a pointer
+ * Return: Return an int
  */
-
-int execute(char *cmd_arr[])
+int execute(char *cmd_array[])
 {
-        pid_t pid;
-        char *exe_path;
-        int status;
+	char *exe_path = NULL;
+	char *cmd = NULL;
+	pid_t pid;
+	int status;
 
-        exe_path = command_path(cmd_arr[0]);
+	cmd = cmd_array[0];
+	exe_path = command_path(cmd);
+	if (exe_path == NULL)
+	{
+		write(2, strcat(cmd, ": Not found\n"), strlen(cmd) + 12);
+		return (3);
+	}
+	pid = fork();
+	if (pid < 0)
+	{
+		perror("Error creating child\n");
+		return (-1);
+	}
+	if (pid > 0)
+		wait(&status);
+	else if (pid == 0)
+	{
+		execve(exe_path, cmd_array, environ);
+		perror("Error");
+		exit(1);
+	}
 
-        if (exe_path == NULL)
-        {
-                fprintf(stderr, "%s: %s: not found", name, cmd_arr[0]);
-                return (3);
-        }
-        pid = fork();
-        if (pid < 0)
-        {
-                perror ("Error ...");
-                return (-1);
-        }
-        if (pid > 0)
-        {
-                wait(&status);
-        }
-        else
-        {
-                if(environ)
-                {
-                        execve(exe_path, cmd_arr, environ);
-                }
-                else
-                {
-                        execve(exe_path, cmd_arr, NULL);
-                }
-        }
-        free (exe_path);
-        return(0);
+	free(exe_path);
+	return (0);
 }
